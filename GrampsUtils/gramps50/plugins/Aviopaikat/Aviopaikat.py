97adc07ead6264238ad8603a1390fad0f5ea8a65
Kari Kujansuu <karikujansuu@gmail.com>
2019-06-09 18:03:15 +0300

    New gramplets

---
+++ GrampsUtils/gramps50/plugins/Aviopaikat/Aviopaikat.py
@@ -0,0 +1,170 @@
+import json
+import pprint
+import re
+
+from gi.repository import Gtk, Gdk, GObject
+
+from gramps.gen.plug import Gramplet
+from gramps.gui.plug import tool
+from gramps.gui.utils import ProgressMeter
+from gramps.gen.db import DbTxn
+from gramps.gen.lib import Place, PlaceRef, PlaceName, PlaceType, Event, EventRef, EventType, Tag
+from gramps.gen.const import GRAMPS_LOCALE as glocale
+
+from gramps.gui.dialog import OkDialog
+
+_ = glocale.translation.gettext
+
+
+# regex helpers
+def p(**kwargs):
+    ret = ""
+    for name,pat in kwargs.items():
+        ret += "(?P<{name}>{pat})".format(name=name,pat=pat)
+    return ret
+
+def optional(pat):
+    return "({pat})?".format(pat=pat)    
+
+def match(s,**kwargs):    
+    pat = p(**kwargs)
+    print(pat)
+    flags = re.VERBOSE
+    r = re.fullmatch(pat,s,flags)
+    if r is None: return None
+    class Ret: pass
+    ret = Ret()
+    ret.__dict__ = r.groupdict()
+    return ret
+
+class Aviopaikat(Gramplet):
+
+    def init(self):
+        self.root = self.__create_gui()
+        self.gui.get_container_widget().remove(self.gui.textview)
+        self.gui.get_container_widget().add_with_viewport(self.root)
+        self.selected_handle = None
+        self.set_tooltip(_("Set properties for multiple places"))
+
+    def db_changed(self):
+        self.__clear(None)
+        
+    def __clear(self, obj):
+        pass
+
+    
+    def __create_gui(self):
+        vbox = Gtk.VBox(orientation=Gtk.Orientation.VERTICAL)
+        vbox.set_spacing(4)
+
+        label = Gtk.Label(_("This gramplet processes marriage places..."))
+        label.set_halign(Gtk.Align.START)
+        label.set_line_wrap(True)
+        vbox.pack_start(label, False, True, 0)
+
+        btn_execute = Gtk.Button(label=_('Execute'))
+        btn_execute.connect("clicked", self.__execute)
+        vbox.pack_start(btn_execute, False, True, 20)
+
+
+        vbox.show_all()
+        return vbox
+
+    
+    def __add_resi_event(self, person, placename, date_object):
+        pn = PlaceName()
+        pn.set_value(placename)
+        place = Place()
+        place.set_name(pn)
+        placehandle = self.dbstate.db.add_place(place, self.trans)
+
+        e = Event()
+        e.set_type(EventType.RESIDENCE)
+        e.set_date_object(date_object)
+        e.set_place_handle(placehandle)
+        eventhandle = self.dbstate.db.add_event(e, self.trans)
+
+        eref = EventRef()
+        eref.ref = eventhandle
+        person.add_event_ref(eref)
+        self.dbstate.db.commit_person(person,self.trans)
+    
+    def __execute(self,obj):
+        with DbTxn(_("Processing marriages"), self.dbstate.db) as self.trans:
+            selected_handles = self.uistate.viewmanager.active_page.selected_handles()
+            num_places = len(selected_handles)
+            for eventhandle in selected_handles:
+                event = self.dbstate.db.get_event_from_handle(eventhandle)
+                print(event)
+                if not event.get_type().is_marriage(): continue
+                placehandle = event.get_place_handle()
+                place = self.dbstate.db.get_place_from_handle(placehandle)
+                pname = place.get_name().value
+                places = self.__match2(pname)
+                if not places: continue
+                print(places)
+                place1, husb_place, wife_place = places
+                family_handles = list(self.dbstate.db.find_backlink_handles(eventhandle,['Family']))
+                print(family_handles)
+                for objtype,family_handle in family_handles:
+                    family = self.dbstate.db.get_family_from_handle(family_handle)
+                    father_handle = family.get_father_handle()
+                    mother_handle = family.get_mother_handle()
+                    if not father_handle or not mother_handle: continue
+                    father = self.dbstate.db.get_person_from_handle(father_handle)
+                    father_name = father.get_primary_name().get_name()
+                    mother = self.dbstate.db.get_person_from_handle(mother_handle)
+                    mother_name = mother.get_primary_name().get_name()
+                    print(father_name,mother_name)
+                    self.__add_resi_event(father,husb_place,event.get_date_object())
+                    self.__add_resi_event(mother,wife_place,event.get_date_object())
+                    pn = PlaceName()
+                    pn.set_value(place1)
+                    newplace = Place()
+                    newplace.set_name(pn)
+                    newplacehandle = self.dbstate.db.add_place(newplace, self.trans)
+                    event.set_place_handle(newplacehandle)
+
+                self.dbstate.db.commit_event(event,self.trans)
+                self.dbstate.db.commit_place(place,self.trans)
+
+    def __match(self,place):
+        m = re.match(r"([^,]+),? ?\(([^/]+)/([^/]+)\)", place)
+        if not m: return False
+        place1 = m.group(1).strip()
+        place2 = m.group(2).strip()
+        place3 = m.group(3).strip()
+        if place2 == "-":
+            husb_place = place1
+        else:
+            husb_place = place2 + ", " + place1
+        if place3 == "-":
+            wife_place = place1
+        else:
+            wife_place = place3 + ", " + place1
+        return place1, husb_place, wife_place
+    
+    def __match2(self,place):
+        non_comma = r'[^,]+'
+        optional_comma = r',?'
+        optional_space = r' ?'
+        leftparen = r'\('
+        rightparen = r'\)'
+        non_slash = r'[^/]+'
+        m = match(place,
+                  place1=non_comma,comma1=optional_comma,space=optional_space,
+                  par1=leftparen,husb_place=non_slash,slash="/",wife_place=non_slash,
+                  par2=rightparen)
+#        r"([^,]+),? ?\(([^/]+)/([^/]+)\)"
+        if not m: return False
+        place2 = m.husb_place
+        place3 = m.wife_place
+        if place2 == "-":
+            husb_place = m.place1
+        else:
+            husb_place = place2 + ", " + m.place1
+        if place3 == "-":
+            wife_place = m.place1
+        else:
+            wife_place = place3 + ", " + m.place1
+        return place1, husb_place, wife_place
---
+++ GrampsUtils/gramps50/plugins/PlaceTool/PlaceTool.py
@@ -0,0 +1,370 @@
+#
+# Gramps - a GTK+/GNOME based genealogy program
+#
+# Copyright (C) 2015-2016 Nick Hall
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, 
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+#
+# SetPlaceProperties
+# ------------------
+# Author: kari.kujansuu@gmail.com
+#
+# Gramplet to change properties of multiple places at the same time.
+# The properties that can be changed are:
+#
+# - place type
+# - tag
+# - enclosing place
+#
+# For the enclosing place first select the place that should enclose the other places 
+# and click the "Set enclosing place" button. The selected place will be displayed.
+# Then select any number of places that should  be enclosed by the first one
+# and click the "Apply to selected places" button. 
+#
+# A place might already have an enclosing place. In that case the new place will be added
+# and the place will end up being enclosed by multiple places. This is quite OK but
+# you can also remove the previous enclosing places by checking the box "Clear original enclosing places".
+# This can e.g. be used to "move" the places under another place.
+#
+# Attempts to set a duplicate enclosing place or a loop (so that a place contains itself) 
+# are quietly bypassed.
+#
+# You can also set the type of the selected place or assign any tag if needed.
+# The operations can be combined so that e.g. the place type and enclosing place can be set 
+# at the same time. Type and tag can be selected from pre-existing ones or you can type
+# a new name if needed.
+#
+# Any existing tags can also be first removed if the "Clear tags" checkbox is marked. Otherwise
+# the new tag is added the set of the tags for the places. 
+#
+# If the enclosing place, type or tag is not specified, then the corresponding
+# setting is not changed.
+#
+# The "Clear selections" button will clear the form.
+#
+# The changes are done under a transaction and the changes can be undone 
+# from the Gramps menu "Edit > Undo Setting place properties". 
+#
+# The "Filter" gramplet can be used to search for the places that need changes. This gramplet
+# does not have direct support for filters.
+#
+# This gramplet can only be added on the Places view.
+ 
+import json
+import pprint
+
+from gi.repository import Gtk, Gdk, GObject
+
+from gramps.gen.plug import Gramplet
+from gramps.gui.plug import tool
+from gramps.gui.utils import ProgressMeter
+from gramps.gen.db import DbTxn
+from gramps.gen.lib import Place, PlaceRef, PlaceName, PlaceType, Note, Tag
+from gramps.gen.const import GRAMPS_LOCALE as glocale
+
+from gramps.gui.dialog import OkDialog
+
+_ = glocale.translation.gettext
+
+class PlaceTool(Gramplet):
+
+    def init(self):
+        self.root = self.__create_gui()
+        self.gui.get_container_widget().remove(self.gui.textview)
+        self.gui.get_container_widget().add_with_viewport(self.root)
+        self.selected_handle = None
+        self.set_tooltip(_("Set properties for multiple places"))
+
+    def db_changed(self):
+        self.__clear(None)
+        
+    def __typenames(self):
+        for pt in self.dbstate.db.get_place_types():
+            yield pt
+        place_type_instance = PlaceType()
+        for pt in place_type_instance.get_standard_names():
+            yield pt
+
+    def __tagnames(self):
+        for handle in self.dbstate.db.get_tag_handles(sort_handles=True):
+            tag = self.dbstate.db.get_tag_from_handle(handle)
+            yield tag.get_name()
+                    
+
+    def __clear(self, obj):
+        self.selected_handle = None        
+        selected_parent = None
+        self.selected_name = ""
+        self.enclosing_place.set_text("None")
+        self.tagcombo.get_child().set_text("")
+        self.typecombo.get_child().set_text("")
+        self.clear_enclosing.set_active(False)
+        self.clear_tags.set_active(False)
+        self.generate_hierarchy.set_active(False)
+        self.spaces.set_active(False)
+        self.reverse.set_active(False)
+    
+
+    
+    def __create_gui(self):
+        vbox = Gtk.VBox(orientation=Gtk.Orientation.VERTICAL)
+        vbox.set_spacing(4)
+
+        label = Gtk.Label(_("This gramplet allows setting properties for multiple places at the same time"))
+        label.set_halign(Gtk.Align.START)
+        label.set_line_wrap(True)
+        vbox.pack_start(label, False, True, 0)
+
+        pt_label = Gtk.Label()
+        pt_label.set_markup("<b>{}</b>".format(_('Place type:')))
+        pt_label.set_halign(Gtk.Align.START)
+        self.typecombo = Gtk.ComboBoxText.new_with_entry()
+        self.__fill_combo(self.typecombo, list(self.__typenames()),wrap_width=4)
+
+        tag_label = Gtk.Label()
+        tag_label.set_markup("<b>{}</b>".format(_('Tag:')))
+        tag_label.set_halign(Gtk.Align.START)
+        self.tagcombo = Gtk.ComboBoxText.new_with_entry()
+        self.__fill_combo(self.tagcombo, list(self.__tagnames()))
+
+        label1 = Gtk.Label()
+        label1.set_markup("<b>{}</b>".format(_("New enclosing place")))
+        label1.set_halign(Gtk.Align.START)
+        label1.set_line_wrap(True)
+        self.label1 = label1
+
+        self.enclosing_place = Gtk.Label(_("None"))
+        self.enclosing_place.set_halign(Gtk.Align.START)
+
+        pt_grid = Gtk.Grid(column_spacing=10)
+        pt_grid.attach(pt_label,0,0,1,1)
+        pt_grid.attach(self.typecombo,1,0,1,1)
+        
+        pt_grid.attach(tag_label,0,1,1,1)
+        pt_grid.attach(self.tagcombo,1,1,1,1)
+        pt_grid.attach(label1,0,2,1,1)
+        pt_grid.attach(self.enclosing_place,1,2,1,1)
+
+        vbox.pack_start(pt_grid, False, True, 0)
+
+        but_set_enclosing = Gtk.Button(label=_('Set enclosing place'))
+        but_set_enclosing.connect("clicked", self.__select)
+        vbox.pack_start(but_set_enclosing, False, True, 20)
+
+        self.clear_enclosing = Gtk.CheckButton(_("Clear original enclosing places"))
+        vbox.pack_start(self.clear_enclosing, False, True, 0)
+
+        self.clear_tags = Gtk.CheckButton(_("Clear tags"))
+        vbox.pack_start(self.clear_tags, False, True, 0)
+
+        self.generate_hierarchy = Gtk.CheckButton(_("Generate hierarchy"))
+        self.generate_hierarchy.connect("clicked", self.__select_generate_hierarchy)
+        vbox.pack_start(self.generate_hierarchy, False, True, 0)
+
+        butbox1 = Gtk.VButtonBox()
+        self.spaces = Gtk.CheckButton(_("use spaces as separator"))
+        self.spaces.set_sensitive(False)
+        butbox1.pack_start(self.spaces, False, True, 0)
+
+        self.reverse = Gtk.CheckButton(_("reverse hierarchy"))
+        self.reverse.set_sensitive(False)
+        butbox1.pack_start(self.reverse, False, True, 0)
+
+        vbox.pack_start(butbox1, False, True, 0)
+
+        but_clear = Gtk.Button(label=_('Clear selections'))
+        but_clear.connect("clicked", self.__clear)
+        vbox.pack_start(but_clear, False, True, 0)
+
+        but_apply = Gtk.Button(label=_('Apply to selected places'))
+        but_apply.connect("clicked", self.__apply)
+        vbox.pack_start(but_apply, False, True, 0)
+
+        vbox.show_all()
+        return vbox
+
+    def __fill_combo(self, combo, data_list, wrap_width=1):
+        for data in sorted(data_list):
+            if data:
+                combo.append_text(data)
+
+        combo.set_popup_fixed_width(False)
+        combo.set_wrap_width(wrap_width)
+        combo.set_entry_text_column(0)
+
+    def __select(self,obj):
+        self.selected_handle = self.uistate.get_active('Place')
+        if not self.selected_handle: 
+            OkDialog(_("Please select a place that will be set as an enclosing place"),
+                     "",
+                     parent=self.uistate.window)
+            return
+        selected_parent = self.dbstate.db.get_place_from_handle(self.selected_handle)
+        self.selected_name = selected_parent.get_name().value + ":" + selected_parent.get_title()
+        self.enclosing_place.set_text(self.selected_name)
+
+    def __select_generate_hierarchy(self,obj):
+        checked = self.generate_hierarchy.get_active()
+        self.spaces.set_sensitive(checked)
+        self.reverse.set_sensitive(checked)
+    
+    def __apply(self,obj):
+        with DbTxn(_("Setting place properties"), self.dbstate.db) as self.trans:
+            tagname = self.tagcombo.get_child().get_text().strip()
+            if tagname:
+                tag = self.__find_tag(tagname)           
+            else:
+                tag = None
+            typename = self.typecombo.get_child().get_text().strip()
+            if typename:
+                ptype = PlaceType(typename)
+            else:
+                ptype = None
+            selected_handles = self.uistate.viewmanager.active_page.selected_handles()
+            num_places = len(selected_handles)
+            x = self.clear_enclosing.get_active()
+            for handle in selected_handles:
+                place = self.dbstate.db.get_place_from_handle(handle)
+                pname = place.get_name().value
+                if self.clear_enclosing.get_active():
+                    self.__clear_enclosing_place(place,handle)
+                if self.clear_tags.get_active():
+                    self.__clear_tags(place,handle)
+                self.__set_enclosing_place(place,handle)
+                if typename: self.__set_type(place,handle,typename)
+                if tag: self.__set_tag(place,handle,tag)
+                if self.generate_hierarchy.get_active():
+                    self.__generate_hierarchy(place,handle)
+
+                self.dbstate.db.commit_place(place,self.trans)
+    
+    def __set_tag(self, place, handle, tag):
+        place.add_tag(tag.handle)
+    
+    def __set_type(self, place, handle,ptype):
+        place.set_type(ptype)
+        
+    def __clear_enclosing_place(self, place, handle):
+        place.set_placeref_list([])
+
+    def __clear_tags(self, place, handle):
+        place.set_tag_list([])
+
+
+    def __encloses(self, handle1, handle2):
+        # True if handle1 encloses handle2 (possibly indirectly)
+        if handle1 == handle2: return True
+        place = self.dbstate.db.get_place_from_handle(handle2)
+        for placeref in place.placeref_list:
+            if self.__encloses(handle1, placeref.ref): return True
+        return False
+    
+    def __set_enclosing_place(self,place,handle):
+        if not self.selected_handle: return
+        if self.__encloses(handle, self.selected_handle):  # place should not include itself
+            print("Can't set",place.get_name().value,"inside",self.selected_name) 
+            return
+        #place = self.dbstate.db.get_place_from_handle(handle)
+        pname = place.get_name().value
+        if self.selected_handle in [r.ref for r in place.placeref_list]: 
+            print(pname,"already enclosed by",self.selected_name)
+            return # prevent duplicates
+        print(pname,"<",self.selected_name)
+        placeref = PlaceRef()
+        placeref.ref = self.selected_handle
+        place.add_placeref(placeref)
+
+
+    def __find_tag(self, name):
+        tag = self.dbstate.db.get_tag_from_name(name)
+        if tag is None: 
+            tag = Tag()                  
+            tag.set_name(name)
+            thandle = self.dbstate.db.add_tag(tag, self.trans)
+            tag = self.dbstate.db.get_tag_from_name(name)
+            self.dbstate.db.commit_tag(tag, self.trans)
+        return tag  
+    
+    def __generate_hierarchy(self,place, handle):
+
+        original_name = place.get_title()
+        #original_name = place.get_name().get_value()
+        separator = ','
+        if ',' not in original_name: return
+
+        names = [name.strip()
+                 for name in original_name.split(separator)
+                 if name.strip()]
+        if self.reverse.get_active(): names.reverse()  
+        place_name = PlaceName()
+        place_name.set_value(names[0])
+        place.set_name(place_name)
+        place.set_title('')
+
+        parent_handle = None
+        for name, handle in self.find_hierarchy(names)[:-1]:
+            if handle is None:
+                new_place = Place()
+                place_name = PlaceName()
+                place_name.set_value(name)
+                new_place.set_name(place_name)
+                if parent_handle is None:
+                    new_place.set_type(PlaceType.COUNTRY)
+                if parent_handle is not None:
+                    placeref = PlaceRef()
+                    placeref.ref = parent_handle
+                    new_place.add_placeref(placeref)
+                parent_handle = self.dbstate.db.add_place(new_place, self.trans)
+            else:
+                parent_handle = handle
+
+        if parent_handle is not None:
+            placeref = PlaceRef()
+            placeref.ref = parent_handle
+            place.add_placeref(placeref)
+
+    def find_hierarchy(self, names):
+        out = []
+        handle = None
+        level = self.get_countries()
+        names.reverse()  # !
+        for name in names:
+            if name not in level:
+                out.append((name, None))
+                level = {}
+            else:
+                handle = level[name]
+                level = self.get_level(handle)
+                out.append((name, handle))
+        return out
+
+    def get_level(self, handle):
+        level = {}
+        for obj, handle in self.dbstate.db.find_backlink_handles(handle, ['Place']):
+            place = self.dbstate.db.get_place_from_handle(handle)
+            level[place.get_name().get_value()] = handle
+        return level
+
+    def get_countries(self):
+        countries = {}
+        for handle in self.dbstate.db.find_place_child_handles(''):
+            place = self.dbstate.db.get_place_from_handle(handle)
+            if int(place.get_type()) != PlaceType.UNKNOWN:
+                countries[place.get_name().get_value()] = handle
+        return countries
+
+
+    
